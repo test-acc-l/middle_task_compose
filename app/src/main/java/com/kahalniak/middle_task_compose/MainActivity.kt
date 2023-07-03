94b2a918518a745afb2048ef44e16c9f6fec31c7
Ruslan Kahalniak <r.kahalniak@lampalampa.net>
2023-07-03 15:58:10 +0300

    Initial commit

---
+++ app/src/main/java/com/kahalniak/middle_task_compose/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.kahalniak.middle_task_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kahalniak.middle_task_compose.ui.theme.Middle_task_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Middle_task_composeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Middle_task_composeTheme {
+        Greeting("Android")
+    }
+}
